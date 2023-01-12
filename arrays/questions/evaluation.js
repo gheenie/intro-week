db1d1708f99a65dd9dbe8d54ddee4c89c0601221
infoomics <sbx_94@hotmail.com>
2023-01-12 17:53:42 +0000

    added a test file for arrays questions

---
+++ arrays/questions/evaluation.js
@@ -0,0 +1,41 @@
+// qn 0
+
+let arr = [];
+arr[3] = "hi";
+
+console.log(arr.length);
+
+// qn 1
+
+const letters = ["a", "b", "c"];
+console.log(letters[2]);
+console.log(letters[1]);
+console.log(letters[0]);
+
+// qn 2
+
+const items = ["hello", , , "there"];
+console.log(items.length);
+
+// qn 3
+
+const people = [["Marie Curie"], ["Albert Einstein"]];
+
+console.log(people[0][0][1]);
+
+// qn 4
+
+arr = [];
+arr[100] = "X";
+
+console.log(arr.length);
+
+// qn 5 part 1
+
+let emptyArray = [];
+console.log(emptyArray[0]);
+
+// qn 5 part 2
+
+emptyArray = [];
+console.log(emptyArray[0][1]);
