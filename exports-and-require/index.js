c1a48b2201b6cf64ab9d7e733ac5cb52662ebe04
infoomics <sbx_94@hotmail.com>
2023-01-13 17:14:54 +0000

    completed examples of exports-and-require

--- exports-and-require/index.js
+++ exports-and-require/index.js
@@ -6,3 +6,26 @@ const somethingFromFile5 = require('./examples/file-5');
 const somethingFromFile6 = require('./examples/file-6');
 
 //console.log(REPLACE_ME_WITH_A_VARIABLE_FROM_ABOVE);
+console.log(somethingFromFile1);
+console.log(somethingFromFile2);
+console.log(somethingFromFile3);
+console.log(somethingFromFile4);
+console.log(somethingFromFile5);
+console.log(somethingFromFile6);
+
+// Section 1
+
+// Vel
+// { tutor: Vel }
+// function printHello
+// object { add: add() }
+// object { printHello: printHello(), add: add() }
+// undefined 
+
+// ans 6 wrong. {}.
+
+// Section 2
+
+console.log(somethingFromFile2.tutor);
+somethingFromFile3();
+somethingFromFile5['printHello']();
